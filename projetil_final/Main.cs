17d9cfd0a58b00258357b2f5bbbf2cb10bbe691a
Amauri Pietropaolo <amauripietropaolo@gmail.com>
2017-11-27 22:10:52 -0200

    Tratando erros com excecao personalizada

--- projetil_final/Main.cs
+++ projetil_final/Main.cs
@@ -8,13 +8,20 @@ namespace projetil_final
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new torre(tab, Cor.black), new Posicao(0, 0));
-            tab.colocarPeca(new torre(tab, Cor.black), new Posicao(1, 3));
-            tab.colocarPeca(new rei(tab, Cor.black), new Posicao(2, 4));
-            Tela.printtab(tab);
-            Console.ReadLine();
+                tab.colocarPeca(new torre(tab, Cor.black), new Posicao(0, 0));
+                tab.colocarPeca(new torre(tab, Cor.black), new Posicao(1, 3));
+                tab.colocarPeca(new rei(tab, Cor.black), new Posicao(0, 2));
+                Tela.printtab(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine(); 
         }
     }
 }
